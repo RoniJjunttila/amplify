f4a0a8bec4e3bbd87255046dde9cc73ec0eef33b
RoniJjunttila <ronijunttilaaaa@gmail.com>
2023-10-03 18:33:09 +0300

    node.js

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// server/index.js
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+//https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/
